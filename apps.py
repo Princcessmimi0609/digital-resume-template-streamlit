20ec3a7b74773791dba1cd1d7eca0d7ac630bbf1
Princcessmimi0609 <150625009+Princcessmimi0609@users.noreply.github.com>
2023-12-26 05:43:25 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "_file_" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "Resume-Anna-Yuen.pdf"
 profile_pic = current_dir / "assets" / "FaJeh.jpg"
 
 
